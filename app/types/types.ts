9acf7a8dfaac2178f3b8705c1981622188ef6a88
NogumaCode <111834765+NogumaCode@users.noreply.github.com>
2024-09-03 00:23:44 +0900

    microcms and logout and  modal function setting

---
+++ app/types/types.ts
@@ -0,0 +1,14 @@
+type BookType = {
+  id:number;
+  title:string;
+  content:string;
+  price:number;
+  thumbnail:{
+    url:string
+  };
+  created_at:string;
+  updated_at:string;
+
+}
+
+export type {BookType};
